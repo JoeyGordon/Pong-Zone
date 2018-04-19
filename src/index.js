77f7cf216b5714e1e9f73a8e136a9b520fc1f125
Tim <tim.denison@gmail.com>
2018-04-19 12:33:50 -0700

    configures redux, creates store, currently console logs app state

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore } from 'redux';
 import * as firebase from 'firebase';
 import App from './app';
 import registerServiceWorker from './registerServiceWorker';
+
+import pongZone from './rootReducer';
+
+const store = createStore(pongZone);
+
+console.log(store.getState());
 
 ReactDOM.render(<App />, global.document.getElementById('root'));
 registerServiceWorker();
