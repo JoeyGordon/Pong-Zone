09380c636994b25b83b62e2cbc65d4f16706d444
ANTHONY ZAVALA <anthony_zavala@homedepot.com>
2018-04-19 11:07:32 -0700

    Created PlayerEloRating model

---
+++ src/Models/PlayerEloRating.js
@@ -0,0 +1,33 @@
+const k = 16;
+const q = (x) => Math.pow(10, (x / 400));
+const eA = (x, y) => q(x) / (q(x) + q(y));
+const privateData = new WeakMap();
+
+export class PlayerEloRating {
+    constructor(eloRating) {
+        privateData.set(this, {
+            eloRating
+        });
+    }
+
+    playerVsPlayerRatingShift(otherPlayerRating, winLoss) {
+        const eloRating = privateData.get(this).eloRating;
+        const shift = Math.round(k * (winLoss - eA(eloRating, otherPlayerRating)));
+
+        const newEloRating = eloRating + shift;
+        privateData.set(this, {
+            eloRating: newEloRating < 0 ? 0 : newEloRating
+        });
+
+        return shift;
+    }
+
+    playerVsTeamRatingShift(oppPairPlayerARating, oppPairPlayerBRating, winLoss) {
+        const shift = this.playerVsPlayerRatingShift((oppPairPlayerARating + oppPairPlayerBRating) / 2, winLoss);
+        return shift;
+    }
+
+    getEloRating() {
+        return privateData.get(this).eloRating;
+    };
+}
