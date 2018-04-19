88a59e9554996eebdfe69d246b4966df208d7ed9
Joey <joey.gordon@gmail.com>
2018-04-19 12:56:20 -0700

    login once

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -4,61 +4,8 @@ import {firebase, db, provider} from '../firebase';
 import * as auth from '../auth';
 
 class SignInForm extends Component {
-    constructor(props) {
-        super(props);
-        this.createUserRecord = this.createUserRecord.bind(this);
-    }
-
     googleLogin() {
         auth.signInWithRedirect(provider);
-    }
-
-    createUserRecord(user) {
-        // console.log("HEY", user)
-        db.collection('users').add(user)
-            .then(function (docRef) {
-                console.log("Document written with ID: ", docRef.id);
-            })
-            .catch(function (error) {
-                console.error("Error adding document: ", error);
-            });
-    }
-
-    componentWillMount() {
-        console.log("HEY THERE")
-        // firebase.auth().onAuthStateChanged(user => {
-        //     if (user) {
-        //         console.log("User signed in: ", user);
-        //     }
-        // });
-
-        // auth.getRedirectResult()
-        // .then(result => {
-        //     if (result.credential && result.additionalUserInfo.isNewUser) {
-        //         const newUser = {
-        //             createdDate: new Date(),
-        //             matches: {},
-        //             rating: 0,
-        //             wins: 0,
-        //             losses: 0,
-        //             name: result.user.displayName,
-        //             email: result.user.email,
-        //             photoUrl: result.user.photoURL,
-        //         }
-        //         this.createUserRecord(newUser);
-        //         console.log(result)
-        //     }
-        // }).catch(function (error) {
-        //     // Handle Errors here.
-        //     var errorCode = error.code;
-        //     var errorMessage = error.message;
-        //     // The email of the user's account used.
-        //     var email = error.email;
-        //     // The firebase.auth.AuthCredential type that was used.
-        //     var credential = error.credential;
-        //     // ...
-        // });
-
     }
 
     render() {
--- src/withUserAuthedAndLoaded.js
+++ src/withUserAuthedAndLoaded.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { firebase, auth } from './firebase';
+import { firebase, auth, db } from './firebase';
 
 // import * as userService from './services/user';
 
@@ -22,8 +22,17 @@ const withUserAuthedAndLoaded = (Component) => {
             }
         }
 
-        componentDidMount() {
+        createUserRecord(user) {
+            db.collection('users').add(user)
+                .then(function (docRef) {
+                    console.log("Document written with ID: ", docRef.id);
+                })
+                .catch(function (error) {
+                    console.error("Error adding document: ", error);
+                });
+        }
 
+        componentDidMount() {
             auth.getRedirectResult()
             .then(result => {
                 if (result.credential && result.additionalUserInfo.isNewUser) {
@@ -38,34 +47,34 @@ const withUserAuthedAndLoaded = (Component) => {
                         photoUrl: result.user.photoURL,
                     }
                     this.createUserRecord(newUser);
-                    console.log(result)
                 }
             }).catch(function (error) {
                 // Handle Errors here.
-                var errorCode = error.code;
-                var errorMessage = error.message;
-                // The email of the user's account used.
-                var email = error.email;
-                // The firebase.auth.AuthCredential type that was used.
-                var credential = error.credential;
-                // ...
+                // var errorCode = error.code;
+                // var errorMessage = error.message;
+                // // The email of the user's account used.
+                // var email = error.email;
+                // // The firebase.auth.AuthCredential type that was used.
+                // var credential = error.credential;
             });
 
             firebase.auth().onAuthStateChanged(user => {
                 if (user) {
                     console.log("User signed in: ", user);
-                    // this.setAuthUserAndLoadUserInfo(authUser)
-                } 
+                    // go fetch the user data from firebase and set the user
+                    // var fetchedUser = db.collection('users').where('email', '==', 'someemail');
+                }
             });
-            const db = firebase.database();
-            this.setAuthUserAndLoadUserInfo = (authUser) => {
-                console.log('authUser', authUser);
-                // const testUser = userService.getUserByEmail(authUser.email);
 
-                db.ref('users').orderByChild('email').equalTo(authUser.email).on('value', (snapshot) => {
-                    this.setState(() => ({ authUser, user: snapshot.val() }));
-                });
-            }
+            // const db = firebase.database();
+            // this.setAuthUserAndLoadUserInfo = (authUser) => {
+            //     console.log('authUser', authUser);
+            //     // const testUser = userService.getUserByEmail(authUser.email);
+
+            //     db.ref('users').orderByChild('email').equalTo(authUser.email).on('value', (snapshot) => {
+            //         this.setState(() => ({ authUser, user: snapshot.val() }));
+            //     });
+            // }
 
 
             // auth.onAuthStateChanged(authUser => {
