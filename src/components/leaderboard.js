3f98a0c433b9c4858aea1c8e86cc2d544bec3e4c
Joey Gordon <joseph_a_gordon@homedepot.com>
2018-04-19 14:05:54 -0700

    photoURL

--- src/components/leaderboard.js
+++ src/components/leaderboard.js
@@ -2,13 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Leaderboard = (props, { authUser }) => {
-  console.log("NOPE")
-return (
-  <div>
-    { authUser && <div>What</div> }
-    <h1>Leaderboard</h1>
-  </div>
-)
+  return (
+    <div>
+      { authUser && <div>What</div> }
+      <h1>Leaderboard</h1>
+    </div>
+  )
 }
 
 Leaderboard.contextTypes = {
