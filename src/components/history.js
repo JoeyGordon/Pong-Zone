a5e7fc9d9052448bce5612b3e706cf1bc23982e4
JoeyGordon <joey.gordon@gmail.com>
2018-04-21 17:59:36 -0700

    match history photos

--- src/components/history.js
+++ src/components/history.js
@@ -1,21 +1,38 @@
+import _ from 'lodash';
 import React from 'react';
-// import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import MatchCard from './matchCard';
 
 const History = (props) => {
+    const matchesArray = props.matches.map(match => {
+        const teamA = _.filter(match.players, ['team', 'A']);
+        const teamB = _.filter(match.players, ['team', 'B']);
+
+        const teamAUsers = teamA.map(player => {
+            return _.find(props.users, ['userId', player.userId])
+        })
+
+        const teamBUsers = teamB.map(player => {
+            return _.find(props.users, ['userId', player.userId])
+        })
+
+        return (
+            <MatchCard match={match} teamA={teamAUsers} teamB={teamBUsers} key={match.matchId} />
+        )
+    });
+
     return (
         <div>
             <h1>History</h1>
-
-            <MatchCard />
-            <MatchCard />
+            {matchesArray}
         </div>
     )
 }
 
-// History.contextTypes = {
-//     authUser: PropTypes.object,
-// };
+const mapStateToProps = state => ({
+    matches: state.matches,
+    users: state.users,
+});
 
-export default History;
+export default connect(mapStateToProps)(History);
