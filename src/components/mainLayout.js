8a9a84d3c90a1e9826d8defc925679a79e4d9de1
Joey Gordon <joseph_a_gordon@homedepot.com>
2018-05-04 16:58:02 -0700

    logging in stuff

--- src/components/mainLayout.js
+++ src/components/mainLayout.js
@@ -37,58 +37,6 @@ const mapStateToProps = state => ({
 
 const MainLayoutWrapper = styled.div`
   min-height: 100%;
-
-  .menu-bar {
-    display: flex;
-    align-items: center;
-    position: fixed;
-    bottom: 0;
-    background: #011627;
-    width: 100%;
-    padding: 1em;
-    color: #fff;
-    z-index: 5;
-  }
-
-  .menu-bar h1 {
-      display: none;
-  }
-
-  .menu-bar ul {
-    display: flex;
-    justify-content: space-between;
-    padding: 0;
-    margin: 0;
-    width: 100%;
-  }
-
-  .menu-bar li {
-    display: block;
-    list-style: none;
-    text-align: center;
-    font-size: 0.8em;
-  }
-
-  .menu-bar li svg {
-      display: block;
-      margin: 0 auto 0.2em;
-  }
-
-  .menu-bar a {
-    color: #fff;
-    text-decoration: none;
-  }
-
-  .menu-bar a:hover {
-    color: #FCD581;
-  }
-
-  .menu-bar a.active {
-    color: #FCD581;
-  }
-
-  .main-content {
-  }
 `;
 
 export default withRouter(connect(mapStateToProps)(MainLayout));
--- src/components/mainMenu.js
+++ src/components/mainMenu.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 
 import * as auth from '../auth';
 import { provider } from '../firebase';
@@ -24,26 +25,23 @@ const MainMenu = (props) => {
         ) :
         null;
 
-    const loginBlock = isLoggedIn ?
-        (
-            <li>
-                <button onClick={auth.doSignOut} className="sign-out-button">Sign Out</button>
-            </li>
-        ) :
-        (
-            <li>
-                <a onClick={(e) => googleLogin(e)} href="">
-                    <i className="fas fa-user fa-2x"></i>
-                    Login
-                </a>
-            </li>
-        )
-
-
-    // const photoBlock = isLoggedIn ? <img className="logged-in-user-photo" src={props.loggedInUser.photoURL} alt="" /> : null;
+    // const loginBlock = isLoggedIn ?
+    //     (
+    //         <li>
+    //             <button onClick={auth.doSignOut} className="sign-out-button">Sign Out</button>
+    //         </li>
+    //     ) :
+    //     (
+    //         <li>
+    //             <a onClick={(e) => googleLogin(e)} href="">
+    //                 <i className="fas fa-user fa-2x"></i>
+    //                 Login
+    //             </a>
+    //         </li>
+    //     )
 
     return (
-        <div className="menu-bar">
+        <MainMenuWrapper>
             <h1>Pong Zone</h1>
 
             <ul className="primary-menu">
@@ -58,16 +56,72 @@ const MainMenu = (props) => {
                     <NavLink to="/history" exact activeClassName="active">
                         <i className="fas fa-list fa-2x"></i>
                         Match History
-                        </NavLink>
+                    </NavLink>
                 </li>
-                {/* {loginBlock} */}
             </ul>
-        </div>
+        </MainMenuWrapper>
     )
 }
 
 const mapStateToProps = state => ({
     loggedInUser: state.user
 });
+
+const MainMenuWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    position: fixed;
+    bottom: 0;
+    background: #011627;
+    width: 100%;
+    padding: 1em;
+    color: #fff;
+    z-index: 5;
+
+    h1 {
+        display: none;
+    }
+
+    ul {
+        display: flex;
+        justify-content: space-between;
+        padding: 0;
+        margin: 0;
+        width: 100%;
+    }
+
+    li {
+        display: block;
+        list-style: none;
+        text-align: center;
+        font-size: 0.8em;
+    }
+
+    li svg {
+        display: block;
+        margin: 0 auto 0.2em;
+    }
+
+    a {
+        color: #fff;
+        text-decoration: none;
+    }
+
+    a:hover {
+        color: #FCD581;
+    }
+
+    a.active {
+        color: #FCD581;
+    }
+
+  @media screen and (min-width: 800px) {
+      .primary-menu {
+        max-width: 800px;
+        margin: 0 auto;
+      }
+  }
+  `;
+
 
 export default withRouter(connect(mapStateToProps)(MainMenu));
